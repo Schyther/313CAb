98a8ee0ad83c99873500495666ad7656414b2837
Schyther <andrei_daniel.safta@stud.acs.upb.ro>
2016-04-05 15:56:50 +0300

    Paleti v0.3
    Probabil o sa mai fie nevoie de functii in interiorul clasei..

--- clase/Paleti.cpp
+++ clase/Paleti.cpp
@@ -1,57 +1,69 @@
-#include<iostream> //nu e versiunea buna, nu lucrati pe vesiunea asta
+#include<iostream>
 #include<string>
 
 using namespace std;
 
+class Produs { //clasa de test (ca sa nu mai dea erori)
+private:
+	int test;
+
+public:
+	Produs() {
+
+	}
+
+	Produs& operator=(const Produs& x) {
+		this->test = x.test;
+	}
+}; //clasa de test
+
 class Palet {
 private:
 	string id_palet;
-	string id_produs;
+	Produs *produs;
 	int nr_itemi;
 	int id_slot;
 
 public:
-
+	
 	Palet() {
 
 	}
 
-	Palet(string id_palet, string id_produs, int nr_itemi, int id_slot) {
+	Palet(string id_palet, int nr_itemi, int id_slot,int prod_type,Produs *produse) {
 		this->id_palet = id_palet;
-		this->id_produs = id_produs;
 		this->nr_itemi = nr_itemi;
 		this->id_slot = id_slot;
+		this->produs = &produse[prod_type];
 	}
-
+	
 	Palet(const Palet& x) {
 		this->id_palet = x.id_palet;
-		this->id_produs = x.id_produs;
 		this->nr_itemi = x.nr_itemi;
 		this->id_slot = x.nr_itemi;
+		this->produs = x.produs; 
 	}
 
 	~Palet() {
 		this->id_palet = '\0';
-		this->id_produs = '\0';
 		this->nr_itemi = 0;
 		this->id_slot = 0;
+		delete[] produs;
 	}
 
 	Palet& operator=(const Palet& x) {
 		this->id_palet = x.id_palet;
-		this->id_produs = x.id_produs;
 		this->nr_itemi = x.nr_itemi;
 		this->id_slot = x.id_slot;
+		this->produs = x.produs;
 		return *this;
 	}
+
 
 	string getIdPalet() {
 		return id_palet;
 	}
 
-	string getIdProdus() {
-		return id_produs;
-	}
 
 	int getNrItems() {
 		return nr_itemi;
@@ -63,10 +75,6 @@ public:
 
 	void setIdPalet(string id_nou) {
 		this->id_palet = id_nou;
-	}
-
-	void setIdProdus(string id_nou) {
-		this->id_produs = id_nou;
 	}
 
 	void setNrItems(int nr_nou) {
@@ -85,13 +93,15 @@ public:
 int main() {
 
 	//test
-	Palet* DEMON=new Palet("1","13",40,55);
-	cout << DEMON->getIdPalet() << " " << DEMON->getIdProdus() << " " << DEMON->getIdSlot() << " " << DEMON->getNrItems() << endl;
+	static Produs *produse = new Produs[100];
 
-	Palet *aux = new Palet("-1", "-1", -1, -1);
+	Palet* DEMON=new Palet("1",40,55,1,produse);
+	cout << DEMON->getIdPalet() << " " << DEMON->getIdSlot() << " " << DEMON->getNrItems() << endl;
+
+	Palet *aux = new Palet("-1", -1, -1,2,produse);
 	DEMON = aux;
-	cout << DEMON->getIdPalet() << " " << DEMON->getIdProdus() << " " << DEMON->getIdSlot() << " " << DEMON->getNrItems() << endl;
-
+	cout << DEMON->getIdPalet() << " " << DEMON->getIdSlot() << " " << DEMON->getNrItems() << endl;
+	
 	DEMON->setIdPalet("10");
 	cout << DEMON->getIdPalet() << endl;
 
