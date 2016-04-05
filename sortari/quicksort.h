235aeb6356986948f570e55c795f6ab6ba5fe343
andreeastoiuu <andreea.stoiu@yahoo.com>
2016-04-05 16:42:38 +0300

    cautare binara + quicksort

---
+++ sortari/quicksort.h
@@ -0,0 +1,39 @@
+template<class T>
+void quicksort(ResizableArray <T2> vector, int pinitial, int pfinal)
+{
+    int m = (pinitial+pfinal) >> 1;
+    int i = pinitial;
+    int j = pfinal;
+    T2 pivot = vector[m];
+
+    while(i<=j)
+    {
+        while(vector[i]<pivot)
+        i++;
+        while(pivot<vector[j])
+        j--;
+
+        if(i<=j)
+        {
+            T2 temp = vector[i];
+    		vector[i] = vector[j];
+    		vector[j] = temp;
+            i++;
+            j--;
+        }
+       }
+       if(pinitial<j)
+       {
+           quicksort(vector, pinitial, j);
+       }
+       if(i<pfinal)
+       {
+            quicksort(vector,i,pfinal);
+       }
+}
+
+
+
+
+
+
