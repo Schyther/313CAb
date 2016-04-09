5fb0725310f0ee8cafe4f837287e062021900caf
Schyther <andrei_daniel.safta@stud.acs.upb.ro>
2016-04-09 21:22:35 +0300

    Update Classes.h

--- Project 1 - Retail Sales Reporting/Project 1 - Retail Sales Reporting/Classes.h
+++ Project 1 - Retail Sales Reporting/Project 1 - Retail Sales Reporting/Classes.h
@@ -473,7 +473,81 @@ public:
 	friend ostream& operator<<(ostream& out, const List<Y> &list);
 };
 
+//Clasa pentru paleti
 
+class Palet {
+private:
+	string idPalet;
+	int idProdus;
+	int nrItemi;
+	int idSlot;
+
+public:
+	
+	Palet() {
+
+	}
+
+	Palet(string idPalet, int nrItemi, int idSlot,Produs produs) {
+		this->idPalet = idPalet;
+		this->nrItemi = nrItemi;
+		this->idSlot = idSlot;
+		this->idProdus = produs.id;
+	}
+	
+	Palet(const Palet& x) {
+		this->idPalet = x.idPalet;
+		this->nrItemi = x.nrItemi;
+		this->idSlot = x.nrItemi;
+		this->idProdus = x.idProdus; 
+	}
+
+	~Palet() {
+		this->idPalet = '\0';
+		this->nrItemi = 0;
+		this->idSlot = 0;
+		this->idProdus= 0;
+	}
+
+	Palet& operator=(const Palet& x) {
+		this->idPalet = x.idPalet;
+		this->nrItemi = x.nrItemi;
+		this->idSlot = x.idSlot;
+		this->idProdus = x.idProdus;
+		return *this;
+	}
+
+
+	string getIdPalet() {
+		return idPalet;
+	}
+
+
+	int getNrItems() {
+		return nrItemi;
+	}
+
+	int getIdSlot() {
+		return idSlot;
+	}
+	
+	int getIdProdus() {
+		return idProdus;
+	}
+
+	void setIdPalet(string id_nou) {
+		this->idPalet = id_nou;
+	}
+
+	void setNrItems(int nr_nou) {
+		this->nrItemi = nr_nou;
+	}
+
+	void setIdSlot(int id_nou) {
+		this->idSlot = id_nou;
+	}
+
+};
 
 //Clasa pentru categorii
 
